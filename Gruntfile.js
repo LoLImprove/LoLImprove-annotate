acaee82bc414b57fcbf666a9bd6cb17d59376b2b
gabriel-dehan <dehan.gabriel@gmail.com>
2014-09-18 17:52:19 +0200

    New file tree + Gruntfile for yannotate component

---
+++ Gruntfile.js
@@ -0,0 +1,125 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  var path = require('path');
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-ember-templates');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-neuter');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src',
+        src: '**',
+        dest: 'build/src/'
+      }
+    },
+
+    emberTemplates: {
+      options: {
+        templateName: function(sourceFile) {
+          return sourceFile.replace(/src\/templates\//, '');
+        }
+      },
+      'build/src/templates.js': ["src/templates/**/*.hbs"]
+    },
+
+    neuter: {
+      options: {
+        includeSourceURL: false,
+        separator: "\n"
+      },
+      "dist/em-yannotate.js":  ["build/src/main.js", "build/src/templates.js"]
+    },
+
+    clean: [
+      "./dist",
+      "./build"
+    ],
+
+    jsdoc: {
+      all: {
+        src: [
+          "./build/src/*.js",
+          "./build/src/**/*.js"
+        ],
+        dest: "doc/"
+      }
+    },
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true,
+          next: true,
+          require: true
+        }
+      },
+      all: ["Gruntfile.js", "build/src/**/*.js"]
+    },
+
+    qunit: {
+      all: ['tests/*.html']
+    },
+
+    uglify: {
+      file: {
+        options: {
+          preserveComments: false,
+          beautify: false,
+          mangle: true,
+          report: 'min'
+        },
+
+        files: {
+          './dist/em-yannotate.min.js': [
+            // Include dist in bundle
+            './dist/em-yannotate.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      grunt: {
+        files: ["Gruntfile.js"],
+        tasks: ["default"]
+      },
+      code: {
+        files: ["src/**/*.js", "dependencies/**/*.js", "vendor/**/*.js"],
+        tasks: ["neuter"]
+      },
+      handlebars: {
+        files: ["src/**/*.hbs"],
+        tasks: ["emberTemplates", "neuter"]
+      },
+      sass: {
+        files: ["src/**/*.scss"],
+        tasks: ["sass"]
+      }
+    }
+  });
+
+  // Build tasks
+  grunt.registerTask("build", ["copy", "emberTemplates", "neuter"]);
+
+  grunt.registerTask("dist", ["build", "uglify"]);
+
+  grunt.registerTask("default", ["dist", "watch"]);
+};
---
+++ dist/em-yannotate.js
@@ -0,0 +1,19 @@
+(function() {
+
+/* Require everything pls */
+
+
+})();
+(function() {
+
+Ember.TEMPLATES["player"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("Playaaaa !");
+  
+});
+
+})();
