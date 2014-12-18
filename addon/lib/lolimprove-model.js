a756295fcef746a77bc1b480d232db5a1a31c2f2
Gabriel Dehan <dehan.gabriel@gmail.com>
2014-12-18 23:44:25 +0100

    More comments

--- addon/lib/lolimprove-model.js
+++ addon/lib/lolimprove-model.js
@@ -44,6 +44,7 @@ Ember.Model.reopen({
 
       if (isHasMany) {
         Ember.addObserver(self, _key, self, function() {
+          // Here we go through each 'empty' relation and set the relationMap
           _.each(this.get( relation + '.content'), function(record) {
             self._logRelationMap(modelName, relation ,id);
             self._setRecordRelationMap(record, modelName, id);
@@ -51,12 +52,16 @@ Ember.Model.reopen({
         });
       } else {
         self.addObserver(_key, self, function() {
+          /* In the case of a belongsTo association we can not access the record
+           * We go through the adapter instead, hooking in when the getParentId is called
+           * to set the relationMap property on the record.
+           */
           var relationAdapter = this.container.lookup('model:' + relation).constructor.adapter;
           var getParentIdFunc = relationAdapter.get('getParentId');
 
           relationAdapter.set('getParentId', function(record, relationName) {
             self._logRelationMap(modelName, relation ,id);
-            self._setRecordRelationMap(record, modelName, id);
+            self._setRecordRelationMap(record, modelName, id); // Maybe conditional ? Need to see how it when record gets updated, etc...
             return getParentIdFunc(record, relationName);
           });
         });
@@ -66,6 +71,7 @@ Ember.Model.reopen({
     this.set('_isMapped', true);
   },
 
+  /* Sets the relationMap property on a record */
   _setRecordRelationMap: function(record, relation, id) {
     var _relations = record.relationsMap || {};
     _relations[Ember.Inflector.inflector.singularize(relation)] = id;
@@ -73,6 +79,7 @@ Ember.Model.reopen({
     return _relations;
   },
 
+  /* Debug log */
   _logRelationMap: function(model, relation, id) {
     console.debug('Setting relationMap for ' + model + '#' + id + ' -> ' + relation);
   },
